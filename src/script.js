ad869c3076c57ca70992e5727856d93825a8c5ca
Sebastian Aguilera <inforaguilera@gmail.com>
2023-12-27 20:59:35 -0300

    detalles css mejorado

--- src/script.js
+++ src/script.js
@@ -1,7 +1,10 @@
 function validarForm(){
+
+    let id = document.getElementById("id-input").value;
+    let stock = document.getElementById("stock-input").value;
     let item = document.getElementById("item-input").value;
 
-    if(item == ""){
+    if(item == "" || id == "" || stock == ""){
         alert("ingrese los datos!")
         return false
     }
@@ -10,7 +13,10 @@ function validarForm(){
 
 function addData(){
     if(validarForm() == true){
+        let id = document.getElementById("id-input").value;
+        let stock = document.getElementById("stock-input").value;
         let item = document.getElementById("item-input").value;
+
         let listaItem;
 
         if(localStorage.getItem('listaItem') == null){
@@ -20,14 +26,16 @@ function addData(){
             listaItem = JSON.parse(localStorage.getItem('listaItem'));
         }
         listaItem.push({
-            item: item
+            id: id,
+            item: item,
+            stock: stock
         });
+
         //transforma un objeto js a una cadena de texto json
         localStorage.setItem("listaItem", JSON.stringify(listaItem))
         //mostrar data
         showData();
         //limpia el input para dejarlo sin texto 
-        document.getElementById("item-input").value="";
         limpiarData();
     }
 }
@@ -44,17 +52,74 @@ function showData(){
     let html = "";
     listaItem.forEach(function(element, index){
         html += "<tr>";
+        html += "<td>" + element.id + "</td>"
         html += "<td>" + element.item + "</td>";
+        html += "<td>" + element.stock + "</td>";
         html += '<td><button onclick="updateData(' + index + ')" class="opcion btn btn-warning">Editar Dato</button> <button onclick="deleteData(' + index + ')" class="opcion btn btn-danger" id="btnDelete">Eliminar Dato</button></td>';
         html += "</tr>";
 
     });
 
     document.querySelector('#dataTable tbody').innerHTML = html;
+
 }
-//crear
+
 document.onload = showData();
 
-// function deleteData(index){
-//    let listItem;
-// }
+
+
+//eliminar
+ function deleteData(index){
+    let listaItem;
+    if(localStorage.getItem("listaItem") == null){
+       listaItem = [];
+    }else{
+        listaItem = JSON.parse(localStorage.getItem("listaItem"));
+    }
+    listaItem.splice(index, 1);
+    localStorage.setItem("listaItem", JSON.stringify(listaItem));
+    showData();
+}
+
+//editar
+function updateData(index){
+  
+    document.getElementById("btnAdd").style.display = "none";
+    document.getElementById("btnUpdate", btnAdd).style.display = "block";
+
+    let listaItem;
+    if(localStorage.getItem("listaItem") == null){
+        listaItem = [];
+    }else{
+         listaItem = JSON.parse(localStorage.getItem("listaItem"));
+         document.querySelector(".block").style.display = "block";
+    }
+    
+    document.getElementById("id-input").value = listaItem[index].id;
+    document.getElementById("item-input").value = listaItem[index].item;
+    document.getElementById("stock-input").value = listaItem[index].stock;
+
+    document.querySelector("#btnUpdate").onclick = function(){
+        if(validarForm()==true){
+          listaItem[index].id = document.getElementById("id-input").value;
+          listaItem[index].item = document.getElementById("item-input").value;
+          listaItem[index].stock = document.getElementById("stock-input").value;
+
+          localStorage.setItem("listaItem", JSON.stringify(listaItem))
+
+          showData();
+          limpiarData()
+          document.querySelector(".block").style.display = "none";
+          document.getElementById("btnAdd").style.display = "block";
+          document.getElementById("btnUpdate", btnAdd).style.display = "none";
+        }
+    }
+
+}
+
+function limpiarData(){
+    document.getElementById("id-input").value = "";
+    document.getElementById("item-input").value = "";
+    document.getElementById("stock-input").value = "";
+}
+
